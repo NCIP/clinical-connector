20f931f4c8f2678657726029c06c56a9157bc56a
Patrick Conrad <conradp@mail.nih.gov>
2010-11-29 17:17:57 +0000

    Removed ClinicalConnector/build directory
    SVN-Revision: 4

--- ClinicalConnector/build/classes/gov/nih/nci/clinicalconnector/dao/HotLinkDAO.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.clinicalconnector.dao;
-
-public interface HotLinkDAO {
-	
-	public String createTransaction(String userName, String password, String study, String patientPosition, String patientId);
-
-}
--- ClinicalConnector/build/classes/gov/nih/nci/clinicalconnector/dao/LoadLabsDAO.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.clinicalconnector.dao;
-
-import gov.nih.nci.cdmsconnector.domain.LoadLabsRequest;
-
-public interface LoadLabsDAO {
-
-	public void loadLabs(LoadLabsRequest request) throws Exception;
-
-}
