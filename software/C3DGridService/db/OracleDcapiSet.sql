d8028d8dafddf6a95604b053fecb5c1930c5946d
Patrick Conrad <conradp@mail.nih.gov>
2011-03-10 19:34:51 +0000

    SVN-Revision: 66

--- software/C3DGridService/db/OracleDcapiSet.sql
+++ software/C3DGridService/db/OracleDcapiSet.sql
@@ -2,24 +2,23 @@ Declare
 
    DEFAULT_ENCRYPTION_KEY	  varchar2(80) := '12C3PR34567890ENCRYPTIONC3PR4KEY5678901234567890';
    DESEDE_ENCRYPTION_SCHEME	  varchar2(10) := 'DESede';
-   DES_ENCRYPTION_SCHEME		varchar2(10) := 'DES';
-   encryptedString varchar2(400);
-
+   DES_ENCRYPTION_SCHEME	  varchar2(10) := 'DES';
 	
 Begin
    Insert into DCAPI_USER_CREDENTIALS (UNAME, PWORD, STATUS, C3D_TNS_ENTRY)
-   Values ('@database.dcapi.user@',
-           getEncrypt('@database.dcapi.user@',DEFAULT_ENCRYPTION_KEY,DES_ENCRYPTION_SCHEME,'1'),
-           'ACTIVE',
-           '@database.name@')
+   select '@database.dcapi.user@',
+          getEncrypt('@database.dcapi.password@',DEFAULT_ENCRYPTION_KEY,DES_ENCRYPTION_SCHEME,'1'),
+          'ACTIVE',
+          '@database.name@'
+   from dual
    where not exists 
          (select 'X' from DCAPI_USER_CREDENTIALS
-           where UNAME = '@database.dcapi.user@');
+           where upper(UNAME) = upper('@database.dcapi.user@'));
 
 
-   Update DCAPI_USER_CREDENTIALS (UNAME, PWORD, STATUS, C3D_TNS_ENTRY)
-      set PWORD         = getEncrypt('@database.dcapi.user@',DEFAULT_ENCRYPTION_KEY,DES_ENCRYPTION_SCHEME,'1'),
+   Update DCAPI_USER_CREDENTIALS
+      set PWORD         = getEncrypt('@database.dcapi.password@',DEFAULT_ENCRYPTION_KEY,DES_ENCRYPTION_SCHEME,'1'),
           STATUS        = 'ACTIVE',
           C3D_TNS_ENTRY = '@database.name@'
-   where UNAME = '@database.dcapi.user@');
+   where upper(UNAME) = upper('@database.dcapi.user@');
 End;
